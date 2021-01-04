14998c792669c47d913c81677db23345e3ec270a
trinhtuan <trinhminhtuan.6868@gmail.com>
2021-01-04 21:32:17 +0700

    modify App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Output from './components/Output.js';
+import Range from './components/Range.js';
 import './App.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: 180,
+      weight: 75,
+      bmi: 23.15,
+      bmiClass: 'Bình Thường',
+      bmiTheme: 'bg-primary'
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  changeHeight = height => {
+    this.setState({
+      height: height
+    }, this.setBmi);
+  };
+
+  changeWeight = weight => {
+    this.setState({
+      weight: weight
+    }, this.setBmi);
+  };
+
+  setBmi = () => {
+    const bmi = (this.state.weight / this.state.height / this.state.height * 10000).toFixed(2);
+    this.setState({
+      bmi: bmi
+    }, this.setClassAndTheme);
+  };
+
+  setClassAndTheme = () => {
+    const bmi = this.state.bmi;
+    if(bmi < 18.5) {
+      this.setState({
+        bmiTheme: 'bg-secondary',
+        bmiClass: 'Thiếu Cân'
+      });
+    } else if(bmi >= 18.5 && bmi < 25) {
+      this.setState({
+        bmiTheme: 'bg-primary',
+        bmiClass: 'Bình Thường'
+      });
+    } else if(bmi >= 25 && bmi < 30) {
+      this.setState({
+        bmiTheme: 'bg-warning',
+        bmiClass: 'Thừa Cân'
+      });
+    } else if(bmi >= 30) {
+      this.setState({
+        bmiTheme: 'bg-danger',
+        bmiClass: 'Béo Phì'
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>BMI Calculator</h1>
+        <div className="height-range">
+          <h3>Height</h3>
+          <Range 
+            callbackChange={ this.changeHeight } 
+            inputValue={ this.state.height }  
+          />
+        </div>
+        <div className="weight-range">
+          <h3>Weight</h3>
+          <Range 
+            callbackChange={ this.changeWeight } 
+            inputValue={ this.state.weight }  
+          />
+        </div>
+        <Output data={ this.state } />
+      </div>
+    );
+  }
 }
 
 export default App;
